19017c3b1019dccd9de255e06d74917df699bc2d
Joseph LaFreniere <joseph@lafreniere.xyz>
2023-04-28 01:29:46 -0500

    aws-codeartifact-proxy: init at 0.5.1

---
+++ pkgs/by-name/aw/aws-codeartifact-proxy/package.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule rec {
+  pname = "aws-codeartifact-proxy";
+  version = "0.5.1";
+
+  src = fetchFromGitHub {
+    owner = "sktan";
+    repo = "aws-codeartifact-proxy";
+    rev = "v${version}";
+    hash = "sha256-289iYPI8J64nRa0PTf47/FQAEqA+rTzalz6S71vFLzs=";
+  };
+  sourceRoot = "${src.name}/src";
+
+  vendorHash = "sha256-3MO+mRCstXw0FfySiyMSs1vaao7kUYIyJB2gAp1IE48=";
+
+  meta = with lib; {
+    description = "An AWS CodeArtifact proxy to allow unauthenticated read access";
+    homepage = "https://github.com/sktan/aws-codeartifact-proxy";
+    license = licenses.mit;
+    maintainers = with maintainers; [ lafrenierejm ];
+  };
+}
