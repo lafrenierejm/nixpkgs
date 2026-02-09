aa35ad0f1655779f0c2e8a95eb838283c8217742
Joseph LaFreniere <git@lafreniere.xyz>
2026-02-08 18:38:31 -0600

    mockgen: Set commit SHA, commit date, and update script

--- pkgs/by-name/mo/mockgen/package.nix
+++ pkgs/by-name/mo/mockgen/package.nix
@@ -2,12 +2,15 @@
   buildGoModule,
   fetchFromGitHub,
   lib,
+  updateGoGitHubModule,
   versionCheckHook,
 }:
 
 buildGoModule (finalAttrs: {
   pname = "mockgen";
   version = "0.6.0";
+  commitSha = "2d1c58167e30f380cf78e44a43b100a14767e817";
+  commitDate = "2025-08-18T14:58:59Z";
 
   src = fetchFromGitHub {
     owner = "uber-go";
@@ -24,13 +27,18 @@ buildGoModule (finalAttrs: {
 
   ldflags = [
     "-X=main.version=${finalAttrs.version}"
-    "-X=main.date=1970-01-01T00:00:00Z"
-    "-X=main.commit=${finalAttrs.src.rev}"
+    "-X=main.date=${finalAttrs.commitDate}"
+    "-X=main.commit=${finalAttrs.commitSha}"
   ];
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgramArg = "-version";
   doInstallCheck = true;
+
+  passthru.updateScript = updateGoGitHubModule {
+    inherit (finalAttrs) pname;
+    inherit (finalAttrs.src) owner repo;
+  };
 
   meta = {
     description = "Mocking framework for the Go programming language";
