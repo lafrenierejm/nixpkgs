cf1f85205d4ee4793811303ae62d62f90a40c689
David McFarland <corngood@gmail.com>
2026-05-06 09:27:18 -0300

    dotnet: fix indentation in update scripts
    (cherry picked from commit f72ac56df28d638720d76b98e845e3855a2031b8)

--- pkgs/development/compilers/dotnet/source/update.sh
+++ pkgs/development/compilers/dotnet/source/update.sh
@@ -3,30 +3,30 @@
 set -euo pipefail
 
 updateSdk() {
-    tag=${1-}
+  tag=${1-}
 
-    if [[ -n $tag ]]; then
-        query=$(cat <<EOF
+  if [[ -n $tag ]]; then
+    query=$(cat <<EOF
             map(
                 select(
                     (.tag_name == "$tag"))) |
             first
 EOF
-        )
-    else
-        sdkVersionPrefix=$channel.1
+         )
+  else
+    sdkVersionPrefix=$channel.1
 
-        query=$(cat <<EOF
+    query=$(cat <<EOF
             map(
                 select(
                     .draft == false and
                     (.tag_name | startswith("v$sdkVersionPrefix")))) |
             first
 EOF
-        )
-    fi
+         )
+  fi
 
-    query="$query "$(cat <<EOF
+  query="$query "$(cat <<EOF
         | (
             .tag_name,
             (.assets |
@@ -39,152 +39,152 @@ EOF
                 select(.name | endswith(".tar.gz.sig")) |
                 .browser_download_url))
 EOF
-    )
+       )
 
-    (
-        curl -fsSL https://api.github.com/repos/dotnet/dotnet/releases | \
-        jq -er "$query" \
-    ) | (
-        read tagName
-        read releaseUrl
-        read sigUrl
+  (
+    curl -fsSL https://api.github.com/repos/dotnet/dotnet/releases | \
+      jq -er "$query" \
+      ) | (
+    read tagName
+    read releaseUrl
+    read sigUrl
 
-        release=${tagName#*.*.}
-        band=${release:0:1}xx
+    release=${tagName#*.*.}
+    band=${release:0:1}xx
 
-        output="$(dirname "${BASH_SOURCE[0]}")"/../"$channel"
+    output="$(dirname "${BASH_SOURCE[0]}")"/../"$channel"
 
-        if [[ $band != 1xx ]]; then
-          output+=/$band
-        fi
+    if [[ $band != 1xx ]]; then
+      output+=/$band
+    fi
 
-        mkdir -p "$output"
+    mkdir -p "$output"
 
-        [[ ! -e "$output"/release.json || $(jq -r .tag "$output"/release.json) != "$tagName" ]] || {
-            >&2 echo "release is already $tagName"
-            exit
-        }
+    [[ ! -e "$output"/release.json || $(jq -r .tag "$output"/release.json) != "$tagName" ]] || {
+      >&2 echo "release is already $tagName"
+      exit
+    }
 
-        # TMPDIR might be really long, which breaks gpg
-        tmp="$(TMPDIR=/tmp mktemp -d)"
-        trap 'rm -rf "$tmp"' EXIT
+    # TMPDIR might be really long, which breaks gpg
+    tmp="$(TMPDIR=/tmp mktemp -d)"
+    trap 'rm -rf "$tmp"' EXIT
 
-        cd "$tmp"
+    cd "$tmp"
 
-        curl -fsSL "$releaseUrl" -o release.json
+    curl -fsSL "$releaseUrl" -o release.json
 
-        tarballUrl=https://github.com/dotnet/dotnet/archive/refs/tags/$tagName.tar.gz
+    tarballUrl=https://github.com/dotnet/dotnet/archive/refs/tags/$tagName.tar.gz
 
-        mapfile -t prefetch < <(nix-prefetch-url --print-path "$tarballUrl")
-        tarballHash=$(nix-hash --to-sri --type sha256 "${prefetch[0]}")
-        tarball=${prefetch[1]}
+    mapfile -t prefetch < <(nix-prefetch-url --print-path "$tarballUrl")
+    tarballHash=$(nix-hash --to-sri --type sha256 "${prefetch[0]}")
+    tarball=${prefetch[1]}
 
-        # recent dotnet 10 releases don't have a signature for the github tarball
-        if [[ ! $sigUrl = */dotnet-source-* ]]; then
-          curl -fssL "$sigUrl" -o release.sig
-          curl -fssLO https://dotnet.microsoft.com/download/dotnet/release-key-2023.asc
-          echo '17aebc401e7999dd0642fa06d23780eba15bd916ebb118454f34db5c85f7194a  release-key-2023.asc' |
-            sha256sum -c
+    # recent dotnet 10 releases don't have a signature for the github tarball
+    if [[ ! $sigUrl = */dotnet-source-* ]]; then
+      curl -fssL "$sigUrl" -o release.sig
+      curl -fssLO https://dotnet.microsoft.com/download/dotnet/release-key-2023.asc
+      echo '17aebc401e7999dd0642fa06d23780eba15bd916ebb118454f34db5c85f7194a  release-key-2023.asc' |
+        sha256sum -c
 
-          (
-              export GNUPGHOME=$PWD/.gnupg
-              mkdir -m 700 -p $GNUPGHOME
-              trap 'gpgconf --kill all' EXIT
-              gpg --no-autostart --batch --import release-key-2023.asc
-              gpg --no-autostart --batch --verify release.sig "$tarball"
-          )
-        fi
+      (
+        export GNUPGHOME=$PWD/.gnupg
+        mkdir -m 700 -p $GNUPGHOME
+        trap 'gpgconf --kill all' EXIT
+        gpg --no-autostart --batch --import release-key-2023.asc
+        gpg --no-autostart --batch --verify release.sig "$tarball"
+      )
+    fi
 
-        tar --strip-components=1 --no-wildcards-match-slash --wildcards -xzf "$tarball" \*/eng/Versions.props \*/global.json \*/prep\*.sh
-        artifactsVersion=$(xq -r '.Project.PropertyGroup |
+    tar --strip-components=1 --no-wildcards-match-slash --wildcards -xzf "$tarball" \*/eng/Versions.props \*/global.json \*/prep\*.sh
+    artifactsVersion=$(xq -r '.Project.PropertyGroup |
             map(select(.PrivateSourceBuiltArtifactsVersion))
             | .[] | .PrivateSourceBuiltArtifactsVersion' eng/Versions.props)
 
-        if [[ "$artifactsVersion" != "" ]]; then
-            artifactVar=$(grep ^defaultArtifactsRid= prep-source-build.sh)
-            eval "$artifactVar"
+    if [[ "$artifactsVersion" != "" ]]; then
+      artifactVar=$(grep ^defaultArtifactsRid= prep-source-build.sh)
+      eval "$artifactVar"
 
-            artifactsFile=Private.SourceBuilt.Artifacts.$artifactsVersion.$defaultArtifactsRid.tar.gz
+      artifactsFile=Private.SourceBuilt.Artifacts.$artifactsVersion.$defaultArtifactsRid.tar.gz
 
-            dotnetBuildUrl=https://builds.dotnet.microsoft.com/
+      dotnetBuildUrl=https://builds.dotnet.microsoft.com/
 
-            if [[ $major -ge 10 ]]; then
-                dotnetBuildUrl+=dotnet/source-build
-            else
-                dotnetBuildUrl+=source-built-artifacts/assets
-            fi
+      if [[ $major -ge 10 ]]; then
+        dotnetBuildUrl+=dotnet/source-build
+      else
+        dotnetBuildUrl+=source-built-artifacts/assets
+      fi
 
-            artifactsUrl=$dotnetBuildUrl/$artifactsFile
+      artifactsUrl=$dotnetBuildUrl/$artifactsFile
 
-            curl -fsSL "$artifactsUrl" --head || {
-              [[ $? == 22 ]]
-              artifactsUrl=https://ci.dot.net/public/source-build/$artifactsFile
-            }
-        else
-            artifactsUrl=$(xq -r '.Project.PropertyGroup |
+      curl -fsSL "$artifactsUrl" --head || {
+        [[ $? == 22 ]]
+        artifactsUrl=https://ci.dot.net/public/source-build/$artifactsFile
+      }
+    else
+      artifactsUrl=$(xq -r '.Project.PropertyGroup |
                 map(select(.PrivateSourceBuiltArtifactsUrl))
                 | .[] | .PrivateSourceBuiltArtifactsUrl' eng/Versions.props)
-            artifactsUrl="${artifactsUrl/dotnetcli.azureedge.net/builds.dotnet.microsoft.com}"
-        fi
+      artifactsUrl="${artifactsUrl/dotnetcli.azureedge.net/builds.dotnet.microsoft.com}"
+    fi
 
-        artifactsHash=$(nix-prefetch-url "$artifactsUrl")
-        artifactsHash=$(nix-hash --to-sri --type sha256 "$artifactsHash")
+    artifactsHash=$(nix-prefetch-url "$artifactsUrl")
+    artifactsHash=$(nix-hash --to-sri --type sha256 "$artifactsHash")
 
-        sdkVersion=$(jq -er .tools.dotnet global.json)
+    sdkVersion=$(jq -er .tools.dotnet global.json)
 
-        # below needs to be run in nixpkgs because toOutputPath uses relative paths
-        cd -
+    # below needs to be run in nixpkgs because toOutputPath uses relative paths
+    cd -
 
-        cp "$tmp"/release.json "$output"
+    cp "$tmp"/release.json "$output"
 
-        jq --null-input \
-            --arg _0 "$tarballHash" \
-            --arg _1 "$artifactsUrl" \
-            --arg _2 "$artifactsHash" \
-            '{
-                "tarballHash": $_0,
-                "artifactsUrl": $_1,
-                "artifactsHash": $_2,
-            }' > "$output"/release-info.json
+    jq --null-input \
+       --arg _0 "$tarballHash" \
+       --arg _1 "$artifactsUrl" \
+       --arg _2 "$artifactsHash" \
+       '{
+          "tarballHash": $_0,
+          "artifactsUrl": $_1,
+          "artifactsHash": $_2,
+        }' > "$output"/release-info.json
 
-        if [[ $band == 1xx ]]; then
-            getBootstrap() {
-                pkgs/development/compilers/dotnet/binary/update.sh \
-                    -o "$output"/bootstrap-sdk.nix --sdk "$1" >&2
-            }
+    if [[ $band == 1xx ]]; then
+      getBootstrap() {
+        pkgs/development/compilers/dotnet/binary/update.sh \
+          -o "$output"/bootstrap-sdk.nix --sdk "$1" >&2
+      }
 
-            getBootstrap "$sdkVersion" || if [[ $? == 2 ]]; then
-                >&2 echo "WARNING: bootstrap sdk missing, attempting to bootstrap with self"
-                getBootstrap "$(jq -er .sdkVersion "$tmp"/release.json)"
-            else
-                exit 1
-            fi
+      getBootstrap "$sdkVersion" || if [[ $? == 2 ]]; then
+        >&2 echo "WARNING: bootstrap sdk missing, attempting to bootstrap with self"
+        getBootstrap "$(jq -er .sdkVersion "$tmp"/release.json)"
+      else
+        exit 1
+      fi
 
-            $(nix-build -A dotnetCorePackages.dotnet_$major.vmr.fetch-deps --no-out-link) >&2
-        fi
-    )
+      $(nix-build -A dotnetCorePackages.dotnet_$major.vmr.fetch-deps --no-out-link) >&2
+    fi
+  )
 }
 
 while [ $# -gt 0 ]; do
-    channel="$1"
-    shift
+  channel="$1"
+  shift
 
-    major="${channel%.*}"
+  major="${channel%.*}"
 
-    if [[ $major -lt 8 ]]; then
-        >&2 echo "dotnet $major has no vmr"
+  if [[ $major -lt 8 ]]; then
+    >&2 echo "dotnet $major has no vmr"
+    continue
+  fi
+
+  curl -fsSL https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/"$channel"/releases.json |
+    jq -r '.releases[0].sdks.[] | .version' | {
+    while read sdk; do
+      if [[ $major -lt 10 && $sdk != *.*.1* ]]; then
+        >&2 echo "sdk $sdk has no vmr"
         continue
-    fi
+      fi
 
-    curl -fsSL https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/"$channel"/releases.json |
-        jq -r '.releases[0].sdks.[] | .version' | {
-        while read sdk; do
-            if [[ $major -lt 10 && $sdk != *.*.1* ]]; then
-                >&2 echo "sdk $sdk has no vmr"
-                continue
-            fi
-
-            updateSdk v"$sdk"
-        done
-    }
+      updateSdk v"$sdk"
+    done
+  }
 done
